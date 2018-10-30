de131840e9a7986e67940fc289a789b087a3974b
nimetapaydin <nimetapaydin@gmail.com>
2018-10-30 18:03:19 +0300

    taksi_cagir sistem eklendi.

--- taksi_cagir.php
+++ taksi_cagir.php
@@ -1,40 +1,74 @@
 <?php
-    if (isset($_POST["arac_plaka"]) && isset($_POST["arac_model"])&& isset($_POST["arac_yakit"])) {
+    if (isset($_POST["musteri_adisoyadi"]) && isset($_POST["musteri_telefon"]) && isset($_POST["musteri_adres"])) {
         require './database.php';
-        $query = $db->prepare("INSERT INTO arac SET plaka = ?, model = ?, yakit = ?");
 
-        $insert = $query->execute([
-            $_POST["arac_plaka"],
-            $_POST["arac_model"],
-            $_POST["arac_yakit"],
-        ]);
+        $query = $db->query("SELECT id, tc FROM sofor WHERE aktif = '0'", PDO::FETCH_ASSOC);
 
-        if ($insert){
-            $last_id = $db->lastInsertId();
-            echo "Araç eklendi!";
+        if ($query->rowCount() > 0) {
+            $sofor = $query->fetch();
+
+            $araba = $db->prepare("SELECT * FROM arac WHERE sofor_id = ?");
+            $araba->execute([$sofor["id"]]);
+
+            if ($araba->rowCount() > 0) {
+                // arabası varsa
+                $db->prepare("UPDATE sofor SET aktif = ? WHERE id = ?")->execute(["1", $sofor["id"]]);
+
+                $insert = $db->prepare("INSERT INTO cagri SET sofor_id = ?, musteri_adisoyadi = ?, musteri_telefon = ?, musteri_adres = ?, aktif = ?");
+                $insert = $insert->execute([
+                    $sofor["id"],
+                    $_POST["musteri_adisoyadi"],
+                    $_POST["musteri_telefon"],
+                    $_POST["musteri_adres"],
+                    1
+                ]);
+                
+                if ($insert) {
+                    echo "Araç yola çıkmıştır";
+                }
+                else {
+                    echo "Taksi çağrılırken bir sorun oluştu";
+                }   
+            }
+            else {
+                // arabası yoksa
+                $araba = $db->prepare("SELECT plaka FROM arac WHERE sofor_id IS NULL")->execute([$sofor["id"]])->fetch();
+                if (!$araba) {
+                    echo "Boşta aracımız yoktur";
+                }
+                else {
+                    // o soför'e araç tahsis edilir
+                    $db->prepare("UPDATE arac SET sofor_id = ? WHERE plaka = ?")->execute([$sofor["id"]], $araba["plaka"]);
+
+                    // araba atandığı için soför yola çıkmaya hazır
+                    $db->prepare("UPDATE sofor SET aktif = ? WHERE id = ?")->execute(["1", $sofor["id"]]);
+
+                    echo "Araç yola çıkmıştır";
+                }
+            }
         }
         else {
-            echo "Ekleme işlemi başarısız";
+            echo "Boşta aracımız yoktur";
         }
     }
 
 ?>
-<h2>Araç ekleme</h2>
+<h2>Taksi Çağır</h2>
 
 <form action="" method="POST">
     <table>
         <tbody>
             <tr>
-                <td>Aktif Şoförler</td>
-                <td><input type="text" name="aktif_sofor" id=""></td>
+                <td>İsminiz</td>
+                <td><input type="text" name="musteri_adisoyadi" id=""></td>
             </tr>
             <tr>
-                <td>Saat</td>
-                <td><input type="text" name="saat" id=""></td>
+                <td>Telefonunuz</td>
+                <td><input type="text" name="musteri_telefon" id=""></td>
             </tr>
             <tr>
-                <td>Adres</td>
-                <td><input type="text" name="adres" id=""></td>
+                <td>Adresiniz</td>
+                <td><input type="text" name="musteri_adres" id=""></td>
             </tr>
             <tr>
                 <td></td>
--- taksici.php
+++ taksici.php
@@ -1,5 +1,6 @@
 <?php
     require './config.php';
+    require './database.php';
     session_start();
 
     if (isset($_GET["cikis"])) {
@@ -8,17 +9,42 @@
         die();
     }
 
+    
     if (isset($_SESSION["oturum_acti"]) && $_SESSION["oturum_acti"] == true) {
-        require './database.php';
-        $query = $db->prepare("SELECT * FROM sofor WHERE tc=?");
-
-        $query->execute([
-            $_SESSION["tc"]
+        
+        $sofor = $db->prepare("SELECT * FROM sofor WHERE tc=?");
+        $sofor->execute([$_SESSION["tc"]]);
+        $sofor = $sofor->fetch();
+        
+        $musteri = $db->prepare("SELECT * FROM cagri WHERE sofor_id = ? AND aktif = ?");
+        $musteri->execute([
+            $sofor["id"],
+            1,
         ]);
+            
+        $musteri = $musteri->fetch();
+            
+        if (isset($_GET["tamamla"]) && isset($_POST["alinan_ucret"])) {
+            $sofor_update = $db->prepare("UPDATE sofor SET aktif = '0' WHERE id = ?");
+            $musteri_update = $db->prepare("UPDATE cagri SET aktif = '0' WHERE aktif = '1' AND sofor_id = ?");
+            $rapor_insert = $db->prepare("INSERT INTO rapor SET sofor_id = ?, arac_plaka = ?, tarih = NOW(), kazanc = ?");
 
-        $rows = $query->fetchAll(PDO::FETCH_ASSOC);
+            $sofor_update = $sofor_update->execute([
+                $sofor["id"]
+            ]);
 
-        $sofor = $rows[0];
+            $musteri_update = $musteri_update->execute([
+                $sofor["id"]
+            ]);
+
+            $rapor_insert->execute([
+                $sofor["id"],
+            ]);
+
+
+
+            die();
+        }
 
 ?>
 <table border='1'>
@@ -31,6 +57,33 @@
             <td>Şuanki Durumunuz:</td>
             <td><?=$sofor["aktif"] == 0 ? "Şu an boştasınız" : "Şu an çalışmaktasınız"?></td>
         </tr>
+        <?php
+        if ($musteri) {
+        ?>
+        <tr>
+            <td>Müşterinizin adı</td>
+            <td><?=$musteri["musteri_adisoyadi"]?></td>
+        </tr>
+        <tr>
+            <td>Müşterinizin telefonu</td>
+            <td><?=$musteri["musteri_telefon"]?></td>
+        </tr>
+        <tr>
+            <td>Müşterinizin adresi</td>
+            <td><?=$musteri["musteri_adres"]?></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <form action="?tamamla=true" method="POST">
+                    Alınan ücret: <input type="text" name="alinan_ucret" id="">
+                    <button>İşi Tamamla</button>
+                </form>
+            </td>
+        </tr>
+        <?php
+        }
+        ?>
         <tr>
             <td></td>
             <td><a href="?cikis=true">Çıkış Yap</a></td>
